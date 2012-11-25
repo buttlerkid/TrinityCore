b184f8be4459c4d2347266a44eb5020be5459d9b
Ellie <ellie1337@gmail.com>
2012-11-25 08:38:27 -0800

    Fixed a compile error in the KillStreak system as well as slightly improving it.

--- src/server/scripts/Custom/KillStreak.cpp
+++ src/server/scripts/Custom/KillStreak.cpp
@@ -1,9 +1,10 @@
 #include "ScriptPCH.h"
+#include "Language.h"
 
 class System_OnPvPKill : public PlayerScript
 {
-	public:
-		System_OnPvPKill() : PlayerScript("System_OnPvPKill") {}
+public:
+	System_OnPvPKill() : PlayerScript("System_OnPvPKill") {}
 
 	struct SystemInfo
 	{
@@ -11,34 +12,35 @@ class System_OnPvPKill : public PlayerScript
 		uint32 LastGUIDKill;
 	};
 
-static std::map<uint32, SystemInfo> KillingStreak;
-	
-void OnPvPKill(Player *pKiller, Player *pVictim)
+	static std::map<uint32, SystemInfo> KillingStreak;
+
+	void OnPvPKill(Player *pKiller, Player *pVictim)
 	{
-		uint32 kGUID; 
-		uint32 vGUID;
-		kGUID = pKiller->GetGUID();
-		vGUID = pVictim->GetGUID();	
-		if(kGUID == vGUID)
-		{
+		uint32 kGUID = pKiller->GetGUID(); 
+		uint32 vGUID = pVictim->GetGUID();
+
+		if( kGUID == vGUID )
 			return;
-		}
-		if(KillingStreak[kGUID].LastGUIDKill == vGUID)
-		{
+
+		if( KillingStreak[kGUID].LastGUIDKill == vGUID )
 			return;
-		}
-		
+
 		KillingStreak[kGUID].KillStreak++;
 		KillingStreak[vGUID].KillStreak = 0;
 		KillingStreak[kGUID].LastGUIDKill = vGUID;
 		KillingStreak[vGUID].LastGUIDKill = 0;
-		
-		switch(KillingStreak[kGUID].KillStreak)
+
+		switch( KillingStreak[kGUID].KillStreak )
 		{
-			case 10:
+		case 5:
+		case 10:
+		case 20:
+		case 50:
+		case 75:
+		case 100:
 			char msg[500];
-			sprintf(msg, "[PvP System]: %s killed %s and is on a 10 kill streak. ", pKiller->GetName(), pVictim->GetName());
-			sWorld.SendWorldText(LANG_SYSTEMMESSAGE, msg);
+			sprintf(msg, "[PvP System]: %s killed %s and is on a %u kill streak.", pKiller->GetName().c_str(), pVictim->GetName().c_str(), KillingStreak[kGUID].KillStreak);
+			sWorld->SendWorldText(LANG_SYSTEMMESSAGE, msg);
 			break;
 		}
 	}
@@ -46,5 +48,5 @@ void OnPvPKill(Player *pKiller, Player *pVictim)
 
 void AddSC_System()
 {
-    new System_OnPvPKill;
+	new System_OnPvPKill;
 }
